975871c6da5dbe94a3dab321adfbf54f02e96f80
Philip <p.kenneweg@unitybox.de>
2024-09-12 12:56:39 +0200

    zeug

--- ee_data.py
+++ ee_data.py
@@ -1,5 +1,6 @@
 import ee
-
+import numpy as np
+import matplotlib.pyplot as plt
 ee.Authenticate()
 
 ee.Initialize(project='ee-philipkenneweg')
@@ -12,6 +13,8 @@ lst = ee.ImageCollection('MODIS/006/MOD11A1')
 
 # Import the USGS ground elevation image.
 elv = ee.Image('USGS/SRTMGL1_003')
+
+sl2 = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED") # Sentinel 2
 
 # Initial date of interest (inclusive).
 i_date = '2017-01-01'
@@ -26,3 +29,22 @@ lst = lst.select('LST_Day_1km', 'QC_Day').filterDate(i_date, f_date)
 u_lon = 8.5325
 u_lat = 52.03202
 u_poi = ee.Geometry.Point(u_lon, u_lat)
+
+roi = u_poi.buffer(1e4)
+
+sl2_img = sl2.select("B2", "B3", "B4").filterBounds(roi).filterDate(i_date, f_date).mean()
+
+
+from IPython.display import Image
+
+# Create a URL to the styled image for a region around France.
+url = sl2_img.getThumbUrl({'dimensions': 512, 'region': roi, 'bands': 'B4,B3,B2'})
+print(url)
+
+# Display the thumbnail land surface temperature in France.
+print('\nPlease wait while the thumbnail loads, it may take a moment...')
+img = Image(url=url)
+
+plt.imshow(np.asarray(img))
+plt.show()
+
